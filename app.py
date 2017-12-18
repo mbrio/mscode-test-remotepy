c6ed3098ceecb5b8a7e9b0b23c31063a89803ba9
Michael Diolosa <michael.diolosa@gmail.com>
2017-12-18 13:06:42 -0500

    Change up configuration

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import tensorflow as tf
 import ptvsd
 
-ptvsd.enable_attach('my_special_secret', address=('0.0.0.0', 3000))
-ptvsd.wait_for_attach()
+print('Starting PTVSD')
+
+ptvsd.enable_attach('my_special_secret', address=('localhost', 50001))
+# ptvsd.wait_for_attach()
+
+print('Starting Application')
 
 node1 = tf.constant(3.0, dtype=tf.float32)
 node2 = tf.constant(4.0)
