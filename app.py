fa84579634c96d1c8e0de3b45a6bfaa35d7d6757
Michael Diolosa <michael.diolosa@gmail.com>
2017-12-18 12:41:31 -0500

    Remote debug

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import tensorflow as tf
+import ptvsd
+
+ptvsd.enable_attach('my_special_secret, address=('192.168.86.156', 3000))
+ptvsd.wait_for_attach()
 
 node1 = tf.constant(3.0, dtype=tf.float32)
 node2 = tf.constant(4.0)
