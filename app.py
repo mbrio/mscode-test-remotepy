9496393e9ae15622520ebeed93178c22cdfe0257
Michael Diolosa <michael.diolosa@gmail.com>
2018-01-03 12:07:24 -0500

    Change port

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import time
 import ptvsd
 
 
-ptvsd.enable_attach('my_secret', address=('localhost', 3000))
+ptvsd.enable_attach('my_secret', address=('localhost', 40000))
 # ptvsd.wait_for_attach()
 
 
